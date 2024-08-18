c9a4ad906edebf86bc3c311e3bdc6a72977f2656
Anderson <trkz.anderson@gmail.com>
2024-08-18 19:03:55 -0300

    feat: add quiz data

---
+++ data/quizData.ts
@@ -0,0 +1,84 @@
+const quizData = [
+  {
+    question: "Quem pode doar sangue para um receptor do tipo A+?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["A+", "A-", "O+", "O-"],
+  },
+  {
+    question: "Quem pode receber sangue de um doador do tipo A+?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["A+", "AB+"],
+  },
+  {
+    question: "Quem pode doar sangue para um receptor do tipo A-?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["A-", "O-"],
+  },
+  {
+    question: "Quem pode receber sangue de um doador do tipo A-?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["A+", "A-", "AB+", "AB-"],
+  },
+  {
+    question: "Quem pode doar sangue para um receptor do tipo B+?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["B+", "B-", "O+", "O-"],
+  },
+  {
+    question: "Quem pode receber sangue de um doador do tipo B+?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["B+", "AB+"],
+  },
+  {
+    question: "Quem pode doar sangue para um receptor do tipo B-?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["B-", "O-"],
+  },
+  {
+    question: "Quem pode receber sangue de um doador do tipo B-?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["B+", "B-", "AB+", "AB-"],
+  },
+  {
+    question: "Quem pode doar sangue para um receptor do tipo AB+?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+  },
+  {
+    question: "Quem pode receber sangue de um doador do tipo AB+?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["AB+"],
+  },
+  {
+    question: "Quem pode doar sangue para um receptor do tipo AB-?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["A-", "B-", "AB-", "O-"],
+  },
+  {
+    question: "Quem pode receber sangue de um doador do tipo AB-?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["AB-", "AB+"],
+  },
+  {
+    question: "Quem pode doar sangue para um receptor do tipo O+?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["O+", "O-"],
+  },
+  {
+    question: "Quem pode receber sangue de um doador do tipo O+?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["O+", "A+", "B+", "AB+"],
+  },
+  {
+    question: "Quem pode doar sangue para um receptor do tipo O-?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["O-"],
+  },
+  {
+    question: "Quem pode receber sangue de um doador do tipo O-?",
+    options: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    correct_options: ["O+", "O-", "A+", "A-", "B+", "B-", "AB+", "AB-"],
+  }
+];
+
+export default quizData;
